82e38eb97de5493c21deff08c3fde53cf1999cec
Rajeev <rpothuraju@miraclesoft.com>
2021-03-25 00:40:51 -0500

    Added JWT Impl and View Templates

--- src/main/java/com/mss/auth/controller/UserController.java
+++ src/main/java/com/mss/auth/controller/UserController.java
@@ -1,17 +1,68 @@
 package com.mss.auth.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mss.auth.config.JwtTokenUtil;
+import com.mss.auth.jwt.JwtRequest;
+import com.mss.auth.jwt.JwtResponse;
+import com.mss.auth.jwt.JwtUserDetailsService;
+import com.mss.auth.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController()
+@Controller
 public class UserController {
 
-    @Value("${spring.security.user.name}")
-    private String userName;
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
-    @GetMapping("/")
-    public String userHello(){
-        return "Hello " + userName.toUpperCase();
-    }
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
+	@Autowired
+	private JwtUserDetailsService userDetailsService;
+
+	@GetMapping("/done")
+	public String add(Model model) {
+		model.addAttribute("jwtRequest", new JwtRequest());
+		return "login";
+	}
+
+	@PostMapping("/done")
+	public ModelAndView createAuthenticationToken(@ModelAttribute("jwtRequest") JwtRequest jwtRequest) throws Exception {
+
+		authenticate(jwtRequest.getUsername(), jwtRequest.getPassword());
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(jwtRequest.getUsername());
+
+		final String token = jwtTokenUtil.generateToken(userDetails);
+
+		JwtResponse response = new JwtResponse(userDetails.getUsername(), token);
+		ModelAndView view = new ModelAndView("register_success");
+		view.addObject("response", response);
+
+		return view;
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public ResponseEntity<?> saveUser(@RequestBody User user) throws Exception {
+		return ResponseEntity.ok(userDetailsService.save(user));
+	}
+
+	private void authenticate(String username, String password) throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
 }
--- src/main/java/com/mss/auth/user/User.java
+++ src/main/java/com/mss/auth/user/User.java
@@ -2,13 +2,16 @@ package com.mss.auth.user;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDto {
+public class User implements Serializable {
     private String username;
     private String password;
+    private String role;
 }
